3af635f354d12a3bf80e497701dd7b8f0607807f
DaveSharma-Hub <davesharma12345@outlook.com>
2024-03-21 14:10:04 -0600

    multiple mongo

--- server/Database/db.js
+++ server/Database/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-const DB_PORT = 27017;
+const DB_PORT = 27019;
 
 const mongodbEndpoint = process.env.DB_ENDPOINT || `mongodb://localhost:${DB_PORT}`;
 
 async function connect(){
     try{
         await mongoose.connect(mongodbEndpoint);
+        console.log("Connected to DB")
     }catch(e){
         console.log(e);
     }
